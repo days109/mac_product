c5e4529e22059b2381258e27ed587057eee4d867
days109 <larstraschen@gmail.com>
2021-06-28 22:56:28 +0800

    write

--- product.py
+++ product.py
@@ -5,9 +5,14 @@ while True:
 	name =  input('請輸入餐點：')
 	if name == 'q':
 		break
-	price = input('請輸入價格：')
+	price = int(input('請輸入價格：'))
 	product.append([name, price])
 print(product[0])
 
 for p in product:
 	print(p[0], '價格是', p[1])
+
+with open('product.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品' + ',' + '價格' + '\n')
+	for p in product:
+		f.write(p[0] + ',' + str(p[1]) +'\n' )
